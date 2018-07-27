ecd150fe7a13934dbb1547fb91aa412dfedf3a3c
Aritra Nirmal <aritranirmal@gmail.com>
2018-07-27 18:46:28 -0400

    test changes

--- bot.go
+++ bot.go
@@ -1,67 +1,69 @@
 package main
 
 import (
-	"os"
 	"fmt"
-	gdax "github.com/preichenberger/go-gdax"
 	"net/http"
+	"os"
 	"time"
-	"strconv"
-  )
 
-  func getTicket(client gdax.Client, ch chan string) {
+	gdax "github.com/preichenberger/go-gdax"
+
+	"strconv"
+)
+
+func getTicket(client gdax.Client, ch chan string) {
 	for {
 		stats, err := client.GetStats("BTC-USD")
 		if err != nil {
-			println(err.Error())  
+			println(err.Error())
 		}
 		ch <- stats.Last
 		time.Sleep(360 * time.Millisecond)
-	  }
-  }
+	}
+}
 
-  func makeBuyOrder(price string){
-	  fmt.Println("Making buy order at: ", price)
-  }
+func makeBuyOrder(price string) {
+	fmt.Println("Making buy order at: ", price)
+}
 
-  func makeSellOrder(price string){
+func makeSellOrder(price string) {
 	fmt.Println("Making sell order at: ", price)
-  }
-  
-  func main(){
+}
+
+func main() {
 	secret := os.Getenv("COINBASE_SECRET")
 	key := os.Getenv("COINBASE_KEY")
 	waitFlg := false
 	//passphrase := os.Getenv("COINBASE_PASSPHRASE")
-	
+
 	// or unsafe hardcode way
 	// secret = "exposedsecret"
 	// key = "exposedkey"
 	passphrase := "covqs3ffw04"
 	client := gdax.NewClient(secret, key, passphrase)
 
-	client.HttpClient = &http.Client {
+	client.HttpClient = &http.Client{
 		Timeout: 15 * time.Second,
-	  }
-	
+	}
+
 	ch := make(chan string)
-    go getTicket(*client, ch)
-	for i:= range ch {
+	go getTicket(*client, ch)
+	for i := range ch {
 		fmt.Println(i)
 		price, err := strconv.ParseFloat(i, 64)
-		if(err != nil && !waitFlg && price <= 7907.42){
+		if err != nil && !waitFlg && price <= 7907.42 {
 			makeBuyOrder(i)
-			waitFlg = true;
+			waitFlg = true
 		}
-		if(err != nil && waitFlg && price >= 7886.43){
+		if err != nil && waitFlg && price >= 7886.43 {
 			makeSellOrder(i)
 			waitFlg = false
 		}
 	}
-	  
+
 	// lastPrice, err := decimal.NewFromString(book.Bids[0].Price)
 	// if err != nil {
-	// 	println(err.Error())  
+	// 	println(err.Error())
 	// }
 
 }
